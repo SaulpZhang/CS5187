974b002c02f783cde04d81a2be8f967b7b855a9f
saulp <saulp_zhang@foxmail.com>
2025-12-04 18:04:03 +0800

    debug

--- train.py
+++ train.py
@@ -16,6 +16,8 @@ import json
 def train_epoch(model, loader, criterion, optimizer, device):
     model.train()
     total_loss = 0.0
+    total_psnr = 0.0
+    total_ssim = 0.0
     for low_imgs, high_imgs in tqdm(loader, desc='Training'):
         low_imgs = low_imgs.to(device)
         high_imgs = high_imgs.to(device)
@@ -30,12 +32,24 @@ def train_epoch(model, loader, criterion, optimizer, device):
         optimizer.step()
         
         total_loss += loss.item()
+
+        # 转为numpy（0-1 → 0-255）
+        outputs_np = outputs.cpu().squeeze().permute(1,2,0).numpy() * 255
+        high_np = high_imgs.cpu().squeeze().permute(1,2,0).numpy() * 255
+        
+        # 计算指标
+        total_psnr += psnr(high_np, outputs_np, data_range=255)
+        total_ssim += ssim(high_np, outputs_np, channel_axis=2, data_range=255)
     
-    return total_loss / len(loader)
+    avg_psnr = total_psnr / len(loader)
+    avg_ssim = total_ssim / len(loader)
+    avg_loss = total_loss / len(loader)
+    return avg_loss, avg_psnr, avg_ssim
 
 # 测试函数（计算PSNR/SSIM）
-def test_epoch(model, loader, device):
+def test_epoch(model, loader,criterion, device):
     model.eval()
+    total_loss = 0.0
     total_psnr = 0.0
     total_ssim = 0.0
     with torch.no_grad():
@@ -45,7 +59,9 @@ def test_epoch(model, loader, device):
             
             # 推理
             outputs = model(low_imgs)
-            
+            loss = criterion(outputs, high_imgs)
+            total_loss += loss.item()
+
             # 转为numpy（0-1 → 0-255）
             outputs_np = outputs.cpu().squeeze().permute(1,2,0).numpy() * 255
             high_np = high_imgs.cpu().squeeze().permute(1,2,0).numpy() * 255
@@ -77,14 +93,26 @@ def main(args):
     # 开始训练
     num_epochs = args.epochs
     best_psnr = 0.0
-    loss = []
-    PSNR = []
-    SSIM = []
+    train_losses = []
+    train_PSNRs = []
+    train_SSIMs = []
+
+    test_losses = []
+    test_PSNRs = []
+    test_SSIMs = []
     for epoch in range(num_epochs):
         print(f'Epoch [{epoch+1}/{num_epochs}]')
-        train_loss = train_epoch(model, train_loader, criterion, optimizer, device)
-        test_psnr, test_ssim = test_epoch(model, test_loader, device)
+        train_loss, train_psnr, train_ssim = train_epoch(model, train_loader, criterion, optimizer, device)
+        test_loss, test_psnr, test_ssim = test_epoch(model, test_loader, criterion, device)
         
+        train_losses.append(train_loss)
+        train_PSNRs.append(train_psnr)
+        train_SSIMs.append(train_ssim)
+
+        test_losses.append(test_loss)
+        test_PSNRs.append(test_psnr)
+        test_SSIMs.append(test_ssim)
+
         # 学习率衰减
         scheduler.step()
         
@@ -98,14 +126,13 @@ def main(args):
         
         print(f'Train Loss: {train_loss:.4f}, Test PSNR: {test_psnr:.2f}, Test SSIM: {test_ssim:.4f}')
         print(f'Best PSNR: {best_psnr:.2f}')
-
-        loss.append(train_loss)
-        PSNR.append(test_psnr)
-        SSIM.append(test_ssim)
     params = {
-        'loss': loss,
-        'PSNR': PSNR,
-        'SSIM': SSIM
+        'train_losses': train_losses,
+        'train_PSNRs': train_PSNRs,
+        'train_SSIMs': train_SSIMs,
+        'test_losses': test_losses,
+        'test_PSNRs': test_PSNRs,
+        'test_SSIMs': test_SSIMs
     }
     with open('./result/train_stats.json', 'w') as f:
         json.dump(params, f)
